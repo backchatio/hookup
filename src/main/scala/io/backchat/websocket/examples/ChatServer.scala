013d94dbb382a74bbba592829d9c77ae4c1c021b
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-03-29 15:27:20 +0200

    small example code cleanup

--- src/main/scala/io/backchat/websocket/examples/ChatServer.scala
+++ src/main/scala/io/backchat/websocket/examples/ChatServer.scala
@@ -2,28 +2,27 @@ package io.backchat.websocket
 package examples
 
 import net.liftweb.json._
-import io.backchat.websocket.WebSocketServer.Exclude
 
 object ChatServer {
 
   implicit val formats: Formats = DefaultFormats
 
-  class ChatServerClient extends WebSocketServerClient {
-    def receive = {
-      case Disconnected(_) =>
-        println("%s has left" format id)
-        this >< ("%s has left" format id, Exclude(this))
-      case Connected =>
-        println("%s has joined" format id)
-        broadcast("%s has joined" format id, Exclude(this))
-      case TextMessage(text) =>
-        println("broadcasting: " + text + " from " + id)
-        this >< (text, Exclude(this))
-    }
-  }
-
   def main(args: Array[String]) {
-    val server = WebSocketServer(ServerInfo("ChatServer", port = 8127))(new ChatServerClient)
+    val server = WebSocketServer(ServerInfo("ChatServer", port = 8127)){
+      new WebSocketServerClient {
+        def receive = {
+          case Disconnected(_) =>
+            println("%s has left" format id)
+            this >< "%s has left".format(id)
+          case Connected =>
+            println("%s has joined" format id)
+            broadcast("%s has joined" format id)
+          case TextMessage(text) =>
+            println("broadcasting: " + text + " from " + id)
+            this >< text
+        }
+      }
+    }
     server.start
   }
 }
