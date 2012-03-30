891c5dba91a3f4f524d1cb98c00812f42d790848
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-03-30 11:21:38 +0200

    Adds spec for the filebuffer

---
+++ src/test/scala/io/backchat/websocket/tests/FileBufferSpec.scala
@@ -0,0 +1,126 @@
+package io.backchat.websocket
+package tests
+
+import org.specs2.time.NoTimeConversions
+import org.specs2.Specification
+import java.io.File
+import org.apache.commons.io.{FilenameUtils, FileUtils}
+import net.liftweb.json.DefaultFormats
+import scala.io.Source
+import collection.JavaConverters._
+import org.specs2.specification.{Fragments, Step}
+import java.util.concurrent.{Executors, ConcurrentLinkedQueue}
+import collection.mutable.ListBuffer
+import akka.dispatch.{Await, Future, ExecutionContext}
+import akka.util.duration._
+import akka.actor.ActorSystem
+import java.util.concurrent.atomic.AtomicInteger
+
+class FileBufferSpec extends Specification with NoTimeConversions { def is =
+  "A FileBuffer should" ^
+    "create the path to the file if it doesn't exist" ! createsPath ^
+    "write to a file while the buffer is open" ! writesToFile ^
+    "write to memory buffer while draining" ! writesToMemory ^
+    "drain the buffers" ! drainsBuffers ^
+    "not fail under concurrent load" ! handlesConcurrentLoads ^
+  end
+
+  implicit val formats = DefaultFormats
+  implicit val executionContext = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
+  override def map(fs: => Fragments) = super.map(fs) ^ Step(executionContext.shutdown())
+
+  def createsPath = {
+    val logPath = new File("./test-work/testing/and/such/buffer.log")
+    val workPath = new File("./test-work")
+    if (workPath.exists()) FileUtils.deleteDirectory(workPath)
+    val buff = new FileBuffer(logPath)
+    buff.open()
+    val res = logPath.getParentFile.exists must beTrue
+    FileUtils.deleteDirectory(workPath)
+    buff.close()
+    res
+  }
+
+  def writesToFile = {
+    FileUtils.deleteQuietly(new File("./test-work2"))
+    val logPath = new File("./test-work2/buffer.log")
+    val buff = new FileBuffer(logPath)
+    val exp1 = TextMessage("the first message")
+    val exp2 = TextMessage("the second message")
+    buff.open()
+    buff.write(exp1)
+    buff.write(exp2)
+    buff.close()
+    val lines = Source.fromFile(logPath).getLines().toList map WebSocket.ParseToWebSocketOutMessage.apply
+    FileUtils.deleteQuietly(new File("./test-work2"))
+    lines must haveTheSameElementsAs(List(exp1, exp2))
+  }
+
+  def writesToMemory = {
+    val logPath = new File("./test-work3/buffer.log")
+    val exp1 = TextMessage("the first message")
+    val exp2 = TextMessage("the second message")
+    val queue = new ConcurrentLinkedQueue[String]()
+    val buff = new FileBuffer(logPath, false, queue)
+    buff.open()
+    buff.write(exp1)
+    buff.write(exp2)
+    val lst = queue.asScala.toList
+    buff.close()
+    FileUtils.deleteDirectory(new File("./test-work3"))
+    lst must haveTheSameElementsAs(List(exp1.content, exp2.content))
+  }
+
+  def drainsBuffers = {
+    val logPath = new File("./test-work4/buffer.log")
+    val buff = new FileBuffer(logPath)
+    val exp1 = TextMessage("the first message")
+    val exp2 = TextMessage("the second message")
+    buff.open()
+    buff.write(exp1)
+    buff.write(exp2)
+    val lines = new ListBuffer[WebSocketOutMessage]
+    Await.ready(buff drain { out =>
+      Future {
+        lines += out
+        Success
+      }
+    }, 5 seconds)
+    buff.close()
+    FileUtils.deleteQuietly(new File("./test-work4"))
+    lines must haveTheSameElementsAs(List(exp1, exp2))
+  }
+
+  def handlesConcurrentLoads = {
+//    val system = ActorSystem("filebufferconc")
+//    val logPath = new File("./test-work5/buffer.log")
+//    val buff = new FileBuffer(logPath)
+//    val lines = new ListBuffer[WebSocketOutMessage]
+//    buff.open()
+//    system.scheduler.scheduleOnce(200 millis) {
+//      Await.ready(buff drain { out =>
+//        Future {
+//          lines += out
+//          Success
+//        }
+//      }, 5 seconds)
+//    }
+//    (1 until 1000) foreach { s =>
+//        Thread.sleep(156)
+//        buff.write(TextMessage("message %s" format s))
+//    }
+//    //Await.ready(Future.sequence(futs), 5 seconds)
+//
+//    Await.ready(buff drain { out =>
+//          Future {
+//            lines += out
+//            Success
+//          }
+//        }, 5 seconds)
+//    buff.close()
+//    FileUtils.deleteQuietly(new File("./test-work5"))
+//    lines must haveSize(1000)
+    pending
+  }
+
+}
