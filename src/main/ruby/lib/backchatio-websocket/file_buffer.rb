304cd2650c84659894ed832ed896e0396536224b
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-05-07 12:22:31 +0200

    Adds spec for file buffer

--- src/main/ruby/lib/backchatio-websocket/file_buffer.rb
+++ src/main/ruby/lib/backchatio-websocket/file_buffer.rb
@@ -19,17 +19,14 @@ module Backchat
 
       def initialize(path, options={})
         @path = path
-        @memory_buffer = []
-        @state = options[:state] || BufferState::Closed
-        @handlers = EM::Channel.new
+        @memory_buffer = options[:memory_buffer] || []
+        @state = options[:initial_state] || BufferState::Closed
+        @handlers = {}
       end
 
       def on(event_name, &cb)
-        @handlers.subscribe do |evt|
-          callback = EM.Callback(&cb)
-          callback.call if evt.size == 1
-          callback.call(evt[1]) if evt[0] == event_name && evt.size > 1
-        end
+        @handlers[event_name] ||= [] 
+        @handlers[event_name] << cb
       end
 
       def remove_on(evt_id) 
@@ -37,68 +34,68 @@ module Backchat
       end
 
       def open 
-        open_file(true) unless @state < BufferState::Opening
+        open_file(true) unless open?
         open?
       end
 
       def open?
-        @state < BufferState::Draining
+        @state > BufferState::Draining
       end
 
       def close 
-        @stream.close if open? and not @stream.closed?
-        @stated = BufferState::Closed
-        emit(:close)
+        if @stream && !@stream.closed? && open?
+          @stream.close 
+          @state = BufferState::Closed
+          emit(:close)
+        end
       end
 
-      def write(message)
+      def write(message, &callback)
         case @state
-        when BufferState::Opening
+        when BufferState::Open
           flush_memory_buffer
-          EM.next_tick do 
-            @stream.write("#{message}\n")
-            yield if block_given?
-          end
+          @stream.puts(message)
+          callback.call if callback
         when BufferState::Closed
           self.open
           write(message)
         else
           @memory_buffer.push message
-          yield if block_given?
+          callback.call if callback
         end
       end
       alias :<< :write
 
       def drain 
         @state = BufferState::Draining
-        EM.next_tick do
-          @stream.close unless @stream.closed?
-          File.open(@path, "r:utf-8") do |file|
-            while(line = file.gets)
-              emit(:data, line.chomp.strip)
-            end
+        @stream.close unless @stream.closed?
+        File.open(@path, "r:utf-8") do |file|
+          while(line = file.gets)
+            emit(:data, line.chomp.strip)
           end
-          open_file false
         end
+        open_file false
       end
 
       private 
-        def emit(evt_name, args = nil)
-          @handlers.publish(args ? [evt_name, args] : [evt_name])
+        def emit(evt_name, *args)
+          (@handlers[evt_name]||[]).each do |cb| 
+            cb.call(*args)
+          end
         end
 
         def open_file(append)
           begin
             @state = BufferState::Opening
             begin
-              FileUtils.mkdir_p(File.dirname(@path))
+              FileUtils.mkdir_p(File.dirname(@path)) unless File.exist?(File.dirname(@path))
             rescue
               @state = BufferState::Closed
               emit(:error, Exception.new("Can't create the path for the file buffer."))  
             end
             @state = BufferState::Open
-            emit(:open) if append
             @stream = File.open(@path, "#{append ? 'a' : 'w'}:utf-8")
+            emit(:open) if append
             flush_memory_buffer
           rescue Exception => e
             @state = BufferState::Closed
@@ -107,7 +104,7 @@ module Backchat
         end
 
         def flush_memory_buffer()
-          @memory_buffer.pop until @memory_buffer.empty?
+          emit(:data, @memory_buffer.pop) until @memory_buffer.empty?
         end
     end
   end
