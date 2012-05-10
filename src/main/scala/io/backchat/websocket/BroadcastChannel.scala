c0f1bef07fc12a6eb75d3f214338061ccf62d67e
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-05-10 13:51:11 +0200

    Adds some scala doc comments

--- src/main/scala/io/backchat/websocket/BroadcastChannel.scala
+++ src/main/scala/io/backchat/websocket/BroadcastChannel.scala
@@ -2,11 +2,32 @@ package io.backchat.websocket
 
 import akka.dispatch.Future
 
+/**
+ * A broadcast channel represents a connection
+ */
 trait BroadcastChannel extends BroadcastChannelLike {
+  /**
+   * @return The connection id
+   */
   def id: Int
 }
 
+/**
+ * Broadcast channel like is either a connection or a proxy for a connection.
+ * It contains the methods for sending and disconnecting from a socket
+ */
 trait BroadcastChannelLike {
+
+  /**
+   * Send a message over the current connection
+   * @param message A [[io.backchat.websocket.WebSocketOutMessage]] message
+   * @return An [[akka.dispatch.Future]] of [[io.backchat.websocket.OperationResult]]
+   */
   def send(message: WebSocketOutMessage): Future[OperationResult]
+
+  /**
+   * Disconnect from the socket, perform closing handshake if necessary
+   * @return An [[akka.dispatch.Future]] of [[io.backchat.websocket.OperationResult]]
+   */
   def disconnect(): Future[OperationResult]
 }
--- src/main/scala/io/backchat/websocket/Throttle.scala
+++ src/main/scala/io/backchat/websocket/Throttle.scala
@@ -3,18 +3,60 @@ package io.backchat.websocket
 import akka.util.Duration
 import akka.util.duration._
 
+/**
+ * The interface for an immutable backoff schedule
+ */
 trait Throttle {
+  /**
+   * The delay to wait until the next operation can occur
+   *
+   * @return A [[akka.util.Duration]]
+   */
   def delay: Duration
+
+  /**
+   * The maximum value the delay can have
+   *
+   * @return A [[akka.util.Duration]]
+   */
   def maxWait: Duration
+
+  /**
+   * Calculate the next delay and return a new throttle
+   * @return A new [[io.backchat.websocket.Throttle]]
+   */
   def next(): Throttle
 }
+
+/**
+ * A Null object representing no backing off at all
+ */
 case object NoThrottle extends Throttle {
 
+  /**
+   * The delay is always 0
+   */
   val delay = 0.millis
+
+  /**
+   * The max wait is always 0
+   */
   val maxWait = 0.millis
 
+  /**
+   * Always returns a [[io.backchat.websocket.NoThrottle]]
+   * @return A [[io.backchat.websocket.NoThrottle]]
+   */
   def next(): Throttle = NoThrottle
 }
+
+/**
+ * Represents a back off strategy that will retry forever when the maximum wait has been reached
+ * From then on it will continue to use the max wait as delay.
+ *
+ * @param delay An [[akka.util.Duration]] indicating how long to wait for the next operation can occur
+ * @param maxWait An [[akka.util.Duration]] indicating the maximum value a `delay` can have
+ */
 case class IndefiniteThrottle(delay: Duration, maxWait: Duration) extends Throttle {
 
   def next(): Throttle = {
@@ -22,6 +64,14 @@ case class IndefiniteThrottle(delay: Duration, maxWait: Duration) extends Thrott
   }
 }
 
+/**
+ * Represents a back off strategy that will retry for `maxTimes` when the maximum wait has been reached
+ * When it can't connect within the `maxTimes` a `maxValue` can occur it will return a [[io.backchat.websocket.NoThrottle]] strategy
+ *
+ * @param delay An [[akka.util.Duration]] indicating how long to wait for the next operation can occur
+ * @param maxWait An [[akka.util.Duration]] indicating the maximum value a `delay` can have
+ * @param maxTimes A [[scala.Int]] representing the maximum amount of time a maxWait can be repeated
+ */
 case class MaxTimesThrottle(delay: Duration, maxWait: Duration, maxTimes: Int = 1) extends Throttle {
 
   def next(): Throttle = {
